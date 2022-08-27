dc5f1f1d3fd5baff0fec38895f7b7076d45d134f
Ronaldo Rocha <ronaldo.devlearning@gmail.com>
2022-08-26 23:07:09 -0300

    Utilizando método get e adicionando endpoint para consulta por id

--- src/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/academy/devdojo/springboot2/controller/AnimeController.java
@@ -3,7 +3,9 @@ package academy.devdojo.springboot2.controller;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,8 +25,13 @@ public class AnimeController {
 	private final DateUtil dateUtil;
 	
 	@GetMapping
-	public List<Anime> list(){
+	public ResponseEntity<List<Anime>> list(){
 		log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-		return animeService.listAll();
+		return ResponseEntity.ok(animeService.listAll());
+	}
+	
+	@GetMapping(path = "/{id}")
+	public ResponseEntity<Anime> findById(@PathVariable long id){
+		return ResponseEntity.ok(animeService.findById(id));
 	}
 }
--- src/academy/devdojo/springboot2/service/AnimeService.java
+++ src/academy/devdojo/springboot2/service/AnimeService.java
@@ -2,14 +2,23 @@ package academy.devdojo.springboot2.service;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import academy.devdojo.springboot2.domain.Anime;
 
 @Service
 public class AnimeService {
+	
+	private List<Anime> animes = List.of(new Anime(1L, "DBZ"), new Anime(2L, "Berserk"), new Anime(3L, "One Peace"));
 
 	public List<Anime> listAll(){
-		return List.of(new Anime(1L, "DBZ"), new Anime(2L, "Berserk"), new Anime(3L, "One Peace"));
+		return animes;
+	}
+	
+	public Anime findById(long id) {
+		return animes.stream().filter(anime -> anime.getId().equals(id))
+				.findFirst().orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
 	}
 }
